3eb59a5baae0e5ab20c2014953b072da194d2ce8
AydinHumphries <48067862+AydinHumphries@users.noreply.github.com>
2020-09-11 20:04:47 +0100

    Add files via upload

---
+++ StreamPeerTCP/Control.gd
@@ -0,0 +1,13 @@
+extends Control
+var socket
+
+func _ready():
+	socket = StreamPeerTCP.new()
+	$buttonConnect.connect("pressed",self,"on_connect")
+	$buttonMessage.connect("pressed",self,"on_message")
+
+func _on_connect():
+	socket.connect_to_host("127.0.0.1", 4242)
+
+func _on_message():
+	socket.put_string("my_message;")
