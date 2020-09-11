4eb5388ad422ee01ab8fdf6baa408b0e784e75c1
AydinHumphries <48067862+AydinHumphries@users.noreply.github.com>
2020-09-11 21:26:47 +0100

    "message;" & "on_connect"/"on_message" spellings
    *only ; presently works to send a string from Gd-Pd, so I removed the character
    *"on_connect" and "on_message" were spelled incorrectly so I changed them to "_on_connect" and "_on_message" to match the function and connection spellings.

--- StreamPeerTCP/Control.gd
+++ StreamPeerTCP/Control.gd
@@ -3,11 +3,11 @@ var socket
 
 func _ready():
 	socket = StreamPeerTCP.new()
-	$buttonConnect.connect("pressed",self,"on_connect")
-	$buttonMessage.connect("pressed",self,"on_message")
+	$buttonConnect.connect("pressed",self,"_on_connect")
+	$buttonMessage.connect("pressed",self,"_on_message")
 
 func _on_connect():
 	socket.connect_to_host("127.0.0.1", 4242)
 
 func _on_message():
-	socket.put_string("my_message;")
+	socket.put_string(";")
